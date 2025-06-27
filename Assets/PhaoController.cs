5a754ca19e4b031316045404ae2350538d0fa747
HuusonDev <thson1602@gmail.com>
2025-06-27 11:35:27 +0700

    add Phao Animation

---
+++ Assets/PhaoController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaoController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        // Lấy Animator từ GameObject này
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu nhấn phím Space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Fire");
+        }
+    }
+}
